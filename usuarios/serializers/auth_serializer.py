7cb7b76a1ecfd6df72c5e83160d5fefb3af93a12
nicoals15 <niokcrack0@gmail.com>
2026-03-09 20:44:03 -0500

    sapa

---
+++ usuarios/serializers/auth_serializer.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if not user:
+                raise serializers.ValidationError("Credenciales inválidas.")
+        else:
+            raise serializers.ValidationError("Debe incluir usuario y contraseña.")
+
+        data['user'] = user
+        return data
+
+
+
+        
